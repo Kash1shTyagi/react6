1af681b07ccf9167f97a82fd092b2b98e29a79d0
Kash1shTyagi <145894360+Kash1shTyagi@users.noreply.github.com>
2023-11-10 19:19:37 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <header>
-      <h1>Dhruv Sharma</h1>
+      <h1>Kashish Tyagi</h1>
       <h2>3rd Year Student at VIT Vellore</h2>
     </header>
   );
